dea27a0298488ac3bad8e77dcc56a15f56b54537
MBSylvain <155894547+MBSylvain@users.noreply.github.com>
2024-07-01 22:36:13 +0200

    ajout de film en fonction des cinéma selectionner version tableau

--- controleurs/horaire.php
+++ controleurs/horaire.php
@@ -13,25 +13,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_cinema = $_POST['id_cinema'];
     $id_film = $_POST['id_film'];
 
-    // Connexion à la base de données
-    $conn = new mysqli("hostname", "username", "password", "database");
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cinema";
 
-    // Vérifier la connexion
-    if ($conn->connect_error) {
-        die("Connexion échouée: " . $conn->connect_error);
-    }
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    // définir le mode d'erreur PDO sur exception
+    $stmt = $pdo->prepare("INSERT INTO Horaires (Jour, Creneau1_Matin, Creneau2_Matin, Creneau3_Matin, Creneau4_Matin, Creneau1_ApresMidi, Creneau2_ApresMidi, Creneau3_ApresMidi, Creneau4_ApresMidi, ID_Cinema, ID_Film) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $id_cinema = ['1', '2', '3'];
 
-    // Préparer et exécuter les insertions
+    // Préparer et exécuter les insertions par jours
     foreach ($jours as $jour) {
-        $sql = "INSERT INTO Horaires (Jour, Creneau1_Matin, Creneau2_Matin, Creneau3_Matin, Creneau4_Matin, Creneau1_ApresMidi, Creneau2_ApresMidi, Creneau3_ApresMidi, Creneau4_ApresMidi, ID_Cinema, ID_Film) VALUES ('$jour', '$creneau1_matin', '$creneau2_matin', '$creneau3_matin', '$creneau4_matin', '$creneau1_apresmidi', '$creneau2_apresmidi', '$creneau3_apresmidi', '$creneau4_apresmidi', $id_cinema, $id_film)";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Nouveau record créé avec succès pour $jour<br>";
-        } else {
-            echo "Erreur: " . $sql . "<br>" . $conn->error;
+        foreach ($id_cinema as $cinema) {
+            $stmt->execute([
+                $jour,
+                $creneau1_matin,
+                $creneau2_matin,
+                $creneau3_matin,
+                $creneau4_matin,
+                $creneau1_apresmidi,
+                $creneau2_apresmidi,
+                $creneau3_apresmidi,
+                $creneau4_apresmidi,
+                $cinema,
+                $id_film
+            ]);
+            echo "Nouveau record créé avec succès pour $jour et cinéma $cinema<br>";
         }
     }
 
     // Fermer la connexion
-    $conn->close();
+
 }
